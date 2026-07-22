3ecb215a64f9b692b80e94cca711983da6d7a201
DOOMvenom <dlalohov233@gmail.com>
2026-07-22 23:16:16 +0500

    Fix `RabbitTypes`

--- src/main/java/com/rosemods/windswept/core/other/WindsweptRabbitTypes.java
+++ src/main/java/com/rosemods/windswept/core/other/WindsweptRabbitTypes.java
@@ -3,12 +3,20 @@ package com.rosemods.windswept.core.other;
 import com.rosemods.windswept.core.Windswept;
 import com.rosemods.windswept.core.other.tags.WindsweptBiomeTags;
 import com.teamabnormals.blueprint.core.api.BlueprintRabbitVariants;
+import com.teamabnormals.blueprint.core.events.LoadThisClassEvent;
 import net.minecraft.core.Holder;
 import net.minecraft.world.level.biome.Biome;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 
+@EventBusSubscriber(modid = Windswept.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class WindsweptRabbitTypes extends BlueprintRabbitVariants {
     public static final BlueprintRabbitVariant CHESTNUT = BlueprintRabbitVariants.register(1227, Windswept.location("chestnut"), context -> {
         Holder<Biome> biome = getBiome(context);
         return biome.is(WindsweptBiomeTags.IS_CHESTNUT_FOREST) || (biome.is(WindsweptBiomeTags.IS_LAVENDER) && context.getFirst().getRandom().nextBoolean());
     });
+
+    @SubscribeEvent
+    public static void $(LoadThisClassEvent event) {
+    }
 }
